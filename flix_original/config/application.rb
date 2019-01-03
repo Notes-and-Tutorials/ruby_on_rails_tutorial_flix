731ef786a953f6bef5d2b432005ef40b06d559f8
Hailee Miu <hailee.miu@gmail.com>
2019-01-03 09:20:14 -0600

    add to github

---
+++ flix_original/config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Flix
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # makes sure that we always have a database connection established when we're in the Rails console
+    console do
+      ActiveRecord::Base.connection
+    end
+  end
+end
